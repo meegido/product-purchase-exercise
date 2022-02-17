feba9d62b65bd4c5a7161cb089c3dcb334ee8c72
javisp91 <javisplanelles@gmail.com>
2022-02-17 09:37:46 +0100

    [Added] test fn

--- src/main/kotlin/domain/Coffee.kt
+++ src/main/kotlin/domain/Coffee.kt
@@ -4,4 +4,8 @@ import java.util.UUID
 
 class Coffee(id: UUID, val items: List<Item>? = emptyList(), name: String, val basePrice: Double) : Product(id, basePrice, name) {
     override fun totalPrice(): Double = (items?.sumOf { it.price } ?: 0.0) + basePrice
+
+    fun greetingTest() {
+        println("I'm a coffee :) ")
+    }
 }
